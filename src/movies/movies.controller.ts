dad4853b79516b7a8faa76e2de6ad18390654c65
Seung-Jong Oh <tmdwhd319@gmail.com>
2024-03-12 04:33:11 +0900

    nest-js controller

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after: ${searchingYear}`;
+  }
+
+  // /:보다 아래에 있으면 id === 'search'로 판단하기에 위로 올려야함.
+  @Get('/:id')
+  getOne(@Param('id') id: string) {
+    return `This will return one movie with the id: ${id}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return movieData;
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie: ${movieId}`;
+  }
+
+  @Patch('/:id')
+  path(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
+  }
+}
